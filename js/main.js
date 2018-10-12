6740a0032fda7e8222da15cbcf28fed412a41e4d
meetcoogle <bhavinder@meetcoogle.com>
2018-10-12 17:40:14 +0530

    Index

---
+++ js/main.js
@@ -0,0 +1,21 @@
+new WOW().init();
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50 ) {
+        $('.scrolltop:hidden').stop(true, true).fadeIn();
+    } else {
+        $('.scrolltop').stop(true, true).fadeOut();
+    }
+});
+$(function(){$(".scroll").click(function(){$("html,body").animate({scrollTop:$("html").offset().top},"1000");return false})})
+
+
+$(document).ready(function () {
+  $('#dtBasicExample').DataTable({
+    "searching": false, // false to disable search (or any other option)
+	"paging": true,
+	"bLengthChange": false,
+  });
+  $('.dataTables_length').addClass('bs-select');
+});	
+	
